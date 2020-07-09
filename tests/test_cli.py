e70c0470c68158df2dddab7a1859ff93826b3df3
Markus Binsteiner <markus@frkl.io>
2020-07-09 11:25:44 +0200

    ci: added 'coverage' step

---
+++ tests/test_cli.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+import pytest
+
+
+@pytest.mark.anyio
+async def test_cli_help():
+
+    pass
+
+    # runner = CliRunner()
+    # result = runner.invoke(main.command)
+
+    # import pp
+    #
+    # pp(result.__dict__)
+    #
+    # assert result.exit_code == 0
+    # assert "xx" in result.output
